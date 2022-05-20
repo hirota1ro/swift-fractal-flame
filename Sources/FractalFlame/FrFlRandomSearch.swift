22862ce54acba479a7aa65c3db5dc6d743fee320
Ichiro HIROTA <ichiro.hirota@gmail.com>
2022-05-20 10:16:40 +0900

    abstraction: random flame generator

--- Sources/FractalFlame/FrFlRandomSearch.swift
+++ Sources/FractalFlame/FrFlRandomSearch.swift
@@ -29,15 +29,20 @@ extension FractalFlame.Search {
           .scaledBy(x: half.width, y: half.height)
           .scaledBy(x: CGFloat(scale), y: CGFloat(scale))
 
+        let generator: FrFlRandomFlameGenerator = useBaseElement
+          ? FrFlRandomFlameGeneratorBase(baseElement: reference)
+          : FrFlRandomFlameGeneratorCount(countOfFrames: reference.flames.count, countOfVarias: reference.varias.count)
+
         let varias = reference.varias
         var found = 0
         var failed = 0
         var threshold = self.threshold
         countProgress.begin()
         while found < count {
-            let flames: [FFFlame] = useBaseElement
-              ? randomFlames(base: reference)
-              : randomFlames(n: reference.flames.count, m: varias.count)
+            // let flames: [FFFlame] = useBaseElement
+            //   ? randomFlames(base: reference)
+            //   : randomFlames(n: reference.flames.count, m: varias.count)
+            let flames: [FFFlame] = generator.randomFlames()
             let elt = FFElement(varias: varias, flames: flames, children: [])
             let ff = elt.fractalFlame
             let stat = FrFlStatSpan()
@@ -77,15 +82,49 @@ extension FractalFlame.Search {
     func fileName(number: Int) -> String {
         return String(format: "%04d", number)
     }
+}
 
-    /// create flames randomly
-    /// - Parameters:
-    ///   - n: the number of F (e.g. 2)
-    ///   - m: the number of V (e.g. 1)
+protocol FrFlRandomFlameGenerator {
+    func randomFlames() -> [FFFlame]
+}
+
+struct FrFlRandomFlameGeneratorBase {
+    let baseElement: FFElement  // the element to start
+}
+
+extension FrFlRandomFlameGeneratorBase: FrFlRandomFlameGenerator {
+    /// create flames randomly (referenced base element)
     /// - Returns: new flames
-    func randomFlames(n: Int, m: Int) -> [FFFlame] {
-        let Δc = 1 / Float(n - 1) // Amount of color to change
-        let array: [FFFlame] = (0 ..< n).map { i -> FFFlame in
+    func randomFlames() -> [FFFlame] {
+        let array: [FFFlame] = baseElement.flames.map { baseFlame in
+            let baseAffine = baseFlame.affine.flattend
+            let a = baseAffine.a + Float.random(in: -0.1 ..< 0.1)
+            let b = baseAffine.b + Float.random(in: -0.1 ..< 0.1)
+            let c = baseAffine.c + Float.random(in: -0.1 ..< 0.1)
+            let d = baseAffine.d + Float.random(in: -0.1 ..< 0.1)
+            let tx = baseAffine.tx + Float.random(in: -0.1 ..< 0.1)
+            let ty = baseAffine.ty + Float.random(in: -0.1 ..< 0.1)
+            let affine = FFAffineMatrix(a:a, b:b, c:c, d:d, tx:tx, ty:ty)
+            let b1: [Float] = baseFlame.blend.map { $0 + Float.random(in: -0.1 ..< 0.1) }
+            let blend = b1
+            let color = baseFlame.color
+            return FFFlame(affine: affine, blend: blend, color: color)
+        }
+        return array
+    }
+}
+
+struct FrFlRandomFlameGeneratorCount {
+    let countOfFrames: Int      // the number of F (e.g. 2)
+    let countOfVarias: Int      // the number of V (e.g. 1)
+}
+
+extension FrFlRandomFlameGeneratorCount: FrFlRandomFlameGenerator {
+    /// create flames randomly
+    /// - Returns: new flames
+    func randomFlames() -> [FFFlame] {
+        let Δc = 1 / Float(countOfFrames - 1) // Amount of color to change
+        let array: [FFFlame] = (0 ..< countOfFrames).map { i -> FFFlame in
             let ang = Float.random(in: -.pi ..< .pi)
             let rot = FFAffineRotation(rotationAngle: ang)
             let tx = Float.random(in: -1.0 ..< 1.0)
@@ -98,32 +137,11 @@ extension FractalFlame.Search {
             let sky = Float.random(in: -1.0 ..< 1.0)
             let skw = FFAffineSkew(skewX: skx, y: sky)
             let affine = FFAffineComposite(array: [skw, rot, scl, tr])
-            let b1: [Float] = (0 ..< m).map { _ in Float.random(in: 0.0 ..< 1.0) }
+            let b1: [Float] = (0 ..< countOfVarias).map { _ in Float.random(in: 0.0 ..< 1.0) }
             let b2 = b1.reduce(0, +)
             let b3 = b1.map { $0 / b2 }
             let blend = b3
             let color = Float(i) * Δc
-            return FFFlame(affine: affine, blend: blend, color: color)
-        }
-        return array
-    }
-
-    /// create flames randomly (referenced base element)
-    /// - Parameter base: Starting element
-    /// - Returns: new flames
-    func randomFlames(base: FFElement) -> [FFFlame] {
-        let array: [FFFlame] = base.flames.map { baseFlame in
-            let baseAffine = baseFlame.affine.flattend
-            let a = baseAffine.a + Float.random(in: -0.1 ..< 0.1)
-            let b = baseAffine.b + Float.random(in: -0.1 ..< 0.1)
-            let c = baseAffine.c + Float.random(in: -0.1 ..< 0.1)
-            let d = baseAffine.d + Float.random(in: -0.1 ..< 0.1)
-            let tx = baseAffine.tx + Float.random(in: -0.1 ..< 0.1)
-            let ty = baseAffine.ty + Float.random(in: -0.1 ..< 0.1)
-            let affine = FFAffineMatrix(a:a, b:b, c:c, d:d, tx:tx, ty:ty)
-            let b1: [Float] = baseFlame.blend.map { $0 + Float.random(in: -0.1 ..< 0.1) }
-            let blend = b1
-            let color = baseFlame.color
             return FFFlame(affine: affine, blend: blend, color: color)
         }
         return array
